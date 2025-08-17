79f05c9ff744d3f73f221479d469f04d48063539
Erik Munkby <erik.munkby@gmail.com>
2025-08-17 09:44:56 +0200

    ci: include extras in install

--- dbt_toolbox/mcp/__init__.py
+++ dbt_toolbox/mcp/__init__.py
@@ -1,13 +1,1 @@
 """Module for the dbt toolbox mcp server."""
-
-from dbt_toolbox import utils
-
-try:
-    import fastmcp  # noqa: F401
-except ModuleNotFoundError:
-    utils.cprint(
-        "Module mcp not found. Install using: ",
-        'pip install "dbt-toolbox[mcp]"',
-        highlight_idx=1,
-        color="red",
-    )
